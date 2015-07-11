86fd1e463bd48fbee84e32df59576e8de9a8d236
MyvarHD <myvarhd@gmail.com>
2015-07-11 11:51:12 +0200

    added links

--- Site/Index.php
+++ Site/Index.php
@@ -6,6 +6,11 @@
 	<body>
 
 		<h1>Entelect Challenge Battle Site</h1>
+		Links:
+		<a href="./BotSetup.php">Set up your bot</a>
+		<a href="./MatchDB.php">View all matches ever played</a>
+		<a href="./BotSetup.php">Leader bords</a>
+		
 	</body>
 
 </html>
