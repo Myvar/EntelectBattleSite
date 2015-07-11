11fca100908b35c52165a9b2dc513511fb95f3a1
MyvarHD <myvarhd@gmail.com>
2015-07-11 13:19:49 +0200

    Added page system

--- Site/views/logged_in.php
+++ Site/views/logged_in.php
@@ -1,21 +1,29 @@
 <?php include('_header.php'); ?>
 
 
-<h1>Entelect Challenge Battle Site</h1>
+<h1><a href="index.php">Entelect Challenge Battle Site</a></h1>
 		Links:<br>
-		<a href="./BotSetup.php">Set up your bot</a><br>
-		<a href="./MatchDB.php">View all matches ever played</a><br>
-		<a href="./BotSetup.php">Leader bords</a><br>
+		<a href="index.php?p=setup">Set up your bot</a><br>
+		<a href="index.php?p=matchdb">View all matches ever played</a><br>
+		<a href="index.php?p=lb">Leader bords</a><br>
 
-<hr>
+
 
 <?php
 // if you need the user's information, just put them into the $_SESSION variable and output them here
-echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . "<br />";
-//echo WORDING_PROFILE_PICTURE . '<br/><img src="' . $login->user_gravatar_image_url . '" />;
-echo WORDING_PROFILE_PICTURE . '<br/>' . $login->user_gravatar_image_tag;
-?>
+echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . "<br /><hr>";
+if(isset($_GET['p']))
+{
+	include('./pages/' . $_GET['p'] . '.php' );
+	
+}
+else
+{
+	include('./pages/home.php' );
+}
 
+?>
+<hr>
 <div>
     <a href="index.php?logout"><?php echo WORDING_LOGOUT; ?></a>
     <a href="edit.php"><?php echo WORDING_EDIT_USER_DATA; ?></a>
