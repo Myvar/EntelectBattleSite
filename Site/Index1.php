7169fbb62ffe13a2efe0a277796b09d301911b9e
MyvarHD <myvarhd@gmail.com>
2015-07-11 11:56:09 +0200

    Added Login system

---
+++ Site/Index1.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Entelect Challenge Battle Site</title>
+	</head>
+	
+	<body>
+
+		<h1>Entelect Challenge Battle Site</h1>
+		Links:
+		<a href="./BotSetup.php">Set up your bot</a>
+		<a href="./MatchDB.php">View all matches ever played</a>
+		<a href="./BotSetup.php">Leader bords</a>
+		
+	</body>
+
+</html>
+
